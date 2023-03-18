3995455b6d68cf03735da9560c7381d56efddc19
Erickkirui <erickkirui653@gmail.com>
2023-03-18 20:12:53 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -7,37 +7,54 @@ function destructivelyAppendCat () {
   return cats.push('Ralph')
 
 }
+destructivelyAppendCat ()
 // add element at beginning of array
 
 function destructivelyPrependCat(){
 
     return cats.unshift("Bob")
 }
+destructivelyPrependCat()
 // remove last element
 function destructivelyRemoveLastCat(){
 
  return cats.pop()   
 }
+destructivelyRemoveLastCat()
 // remove last element 
 function destructivelyRemoveFirstCat(){
     return cats.shift()
 }
-
-const beforeEach =(function () {
-    cats.length = 0;
-  
-    cats.push("Milo", "Otis", "Garfield");
-  });
-
+destructivelyRemoveFirstCat()
 
 
 // new array called cats
+
 function appendCat(){
-    beforeEach();
-    let cat = ""
+    let cat = ''
     return  cat = [...cats,'Broom']
     
 }
 appendCat()
-console.log(appendCat())
 
+
+// prepend cats
+function  prependCat(){
+    let cat = ""
+    return  cat = ["Arnold",...cats]
+}
+prependCat()
+
+//remove the last cat in cats array
+function  removeLastCat(){
+    let catsWithoutLast =""
+    return catsWithoutLast = cats.slice(0, -1)
+}
+removeLastCat()
+console.log(removeLastCat())
+
+//remove cat from first array
+function removeFirstCat(){
+    let catsWithoutfirst = ""
+    return catsWithoutfirst = cats.slice(1)
+}
